740569f2ce6734a0042d6853f070b81af94b92b6
Oleksii Kochurov <aleksei.kochuroff@icloud.com>
2023-06-01 18:44:28 +0300

    styles finished

--- src/components/Card/Tweet.jsx
+++ src/components/Card/Tweet.jsx
@@ -1,11 +1,14 @@
 import {
   TweetBox,
   ContentBox,
+  ImageBox,
   Logo,
   Line,
   AvatarBox,
   Circle,
   Avatar,
+  DataList,
+  Button,
 } from './Tweet.styled';
 import logo from '../../images/logo.svg';
 import picture from '../../images/picturebgr.png';
@@ -16,16 +19,24 @@ export const Tweet = () => {
   return (
     <TweetBox>
       <ContentBox>
-        <div>
+        <ImageBox>
           <Logo src={logo} alt="logo" width="76" height="20"></Logo>
           <img src={picture} alt="background" width="308" height="168"></img>
-        </div>
+        </ImageBox>
         <Line></Line>
         <AvatarBox>
           <Circle src={circle} alt="circle"></Circle>
           <Avatar src={avatar} alt="avatar" />
         </AvatarBox>
-        <div>data</div>
+        <DataList>
+          <li>
+            <p>Tweets</p>
+          </li>
+          <li>
+            <p>Followers</p>
+          </li>
+        </DataList>
+        <Button type="button">Follow</Button>
       </ContentBox>
     </TweetBox>
   );
--- src/components/Card/Tweet.styled.jsx
+++ src/components/Card/Tweet.styled.jsx
@@ -2,10 +2,6 @@ import styled from '@emotion/styled';
 
 export const TweetBox = styled.li`
   position: relative;
-  // display: flex;
-  // flex-direction: column;
-  // justify-content: center;
-  // align-items: center;
   width: 380px;
   height: 460px;
 
@@ -15,12 +11,23 @@ export const TweetBox = styled.li`
     #5736a3 54.28%,
     #4b2a99 78.99%
   );
-  box-shadow: -2.5777px 6.87386px 20.6216px rgba(0, 0, 0, 0.23);
   border-radius: 20px;
+
+  font-weight: 500;
+  font-size: 20px;
+  line-height: 24px;
+  text-transform: uppercase;
+  color: #ebd8ff;
 `;
 
 export const ContentBox = styled.div`
+  display: flex;
+  flex-direction: column;
   padding: 36px 28px;
+`;
+
+export const ImageBox = styled.div`
+  margin-bottom: 88px;
 `;
 
 export const Logo = styled.img`
@@ -58,20 +65,44 @@ export const Circle = styled.img`
   position: absolute;
   top: -6px;
   left: -9px;
+  z-index: 1;
   width: 80px;
   height: 80px;
-  // border: 8px solid #ebd8ff;
   border-radius: 50%;
-  // background: rgb(87 54 163);
-  // box-shadow: 0px 4.39163px 4.39163px rgba(0, 0, 0, 0.06),
-  //   inset 0px -2.19582px 4.39163px #ae7be3,
-  //   inset 0px 4.39163px 3.29372px #fbf8ff;
 `;
 
 export const Avatar = styled.img`
-  position: absolute;
+  // position: absolute;
   width: 62px;
   height: 62px;
-  transform: translateY(-4px);
   object-fit: cover;
+`;
+
+export const DataList = styled.ul`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  margin-bottom: 26px;
+`;
+
+export const Button = styled.button`
+  width: 196px;
+  height: 50px;
+  margin: 0 auto;
+  background: #ebd8ff;
+  border: none;
+  border-radius: 10.3108px;
+  cursor: pointer;
+
+  font-weight: 600;
+  font-size: 18px;
+  line-height: 22px;
+  text-transform: uppercase;
+  color: #373737;
+
+  &:hover {
+    box-shadow: 0px 3.43693px 3.43693px rgba(0, 0, 0, 0.25);
+  }
 `;
